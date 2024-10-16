c92b8e7491d0274c0a6f1cbd56abc0945139c90b
Madushanka24 <madushanka20021224@gmail.com>
2024-10-16 11:57:17 +0530

    CustomerService

---
+++ src/main/java/lk/ijse/ptobackendv2/entity/impl/OrderEntity.java
@@ -0,0 +1,4 @@
+package lk.ijse.ptobackendv2.entity.impl;
+
+public class OrderEntity {
+}
--- src/main/java/lk/ijse/ptobackendv2/service/CustomerService.java
+++ src/main/java/lk/ijse/ptobackendv2/service/CustomerService.java
@@ -1,4 +1,14 @@
 package lk.ijse.ptobackendv2.service;
 
+import lk.ijse.ptobackendv2.dto.impl.CustomerDto;
+
+import java.util.List;
+
 public interface CustomerService {
+    void saveCustomer(CustomerDto customerDto);
+    List<CustomerDto> loadAllCustomers();
+    void deleteCustomers(String customerID);
+    void updateCustomers(String customerID, CustomerDto customerDto);
+    CustomerDto getCustomersByID(String customerId);
+    CustomerDto getCustomersByPhoneNumber(String customerPhoneNumber);
 }

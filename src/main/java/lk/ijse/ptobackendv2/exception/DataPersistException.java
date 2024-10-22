0b13f781442f507bb40d22c9520222f18b14fc1e
Madushanka24 <madushanka20021224@gmail.com>
2024-10-22 12:32:25 +0530

    DataPersist

---
+++ src/main/java/lk/ijse/ptobackendv2/exception/DataPersistException.java
@@ -0,0 +1,14 @@
+package lk.ijse.ptobackendv2.exception;
+
+public class DataPersistException extends RuntimeException {
+
+    public DataPersistException(String message) {
+
+    }
+    public DataPersistException(String message, Throwable cause) {
+
+    }
+    public DataPersistException(Throwable cause) {
+
+    }
+}

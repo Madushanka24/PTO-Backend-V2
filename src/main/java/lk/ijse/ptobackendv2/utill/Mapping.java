1406e6eca60c47464c9a0fa4ed86eea85522c997
Madushanka24 <madushanka20021224@gmail.com>
2024-10-22 12:24:18 +0530

    mapping

---
+++ src/main/java/lk/ijse/ptobackendv2/utill/Mapping.java
@@ -0,0 +1,26 @@
+package lk.ijse.ptobackendv2.utill;
+
+import lk.ijse.ptobackendv2.dto.impl.CustomerDto;
+import lk.ijse.ptobackendv2.entity.impl.CustomerEntity;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class Mapping {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    /*customer mapping*/
+    public CustomerEntity toCustomerEntity(CustomerDto customerDto) {
+        return modelMapper.map(customerDto, CustomerEntity.class);
+    }
+    public CustomerDto toCustomerDto(CustomerEntity customerEntity) {
+        return modelMapper.map(customerEntity, CustomerDto.class);
+    }
+
+    public List<CustomerDto> toCustomerDtoList(List<CustomerEntity> customerEntityList) {
+        return modelMapper.map(customerEntityList, new TypeToken<List<CustomerDto>>() {}.getType());
+    }
+}

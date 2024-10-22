606e012ccd28531ff4c536514085d54ce4cd5e59
Madushanka24 <madushanka20021224@gmail.com>
2024-10-22 12:32:01 +0530

    OrderNotFound

---
+++ src/main/java/lk/ijse/ptobackendv2/exception/OrderNotFoundException.java
@@ -0,0 +1,14 @@
+package lk.ijse.ptobackendv2.exception;
+
+public class OrderNotFoundException extends RuntimeException {
+
+    public OrderNotFoundException() {
+
+    }
+    public OrderNotFoundException(String message) {
+
+    }
+    public OrderNotFoundException(String message, Throwable cause) {
+
+    }
+}

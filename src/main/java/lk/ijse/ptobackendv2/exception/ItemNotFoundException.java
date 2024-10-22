80097a62ce1d8af10118e0c891d52e78fa7bf5a9
Madushanka24 <madushanka20021224@gmail.com>
2024-10-22 12:19:45 +0530

    itemNotFound

---
+++ src/main/java/lk/ijse/ptobackendv2/exception/ItemNotFoundException.java
@@ -0,0 +1,14 @@
+package lk.ijse.ptobackendv2.exception;
+
+public class ItemNotFoundException extends RuntimeException {
+
+    public ItemNotFoundException() {
+
+    }
+    public ItemNotFoundException(String message) {
+
+    }
+    public ItemNotFoundException(String message, Throwable cause) {
+
+    }
+}
